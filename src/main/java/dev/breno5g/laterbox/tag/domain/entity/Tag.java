0bf138f2365f5128d732988cd7080e87c48e9581
Breno Santos <brenosantos145@gmail.com>
2025-08-08 14:56:34 -0300

    chore: add db and flyway

---
+++ src/main/java/dev/breno5g/laterbox/tag/domain/entity/Tag.java
@@ -0,0 +1,27 @@
+package dev.breno5g.laterbox.tag.domain.entity;
+
+import dev.breno5g.laterbox.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tags")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private String color;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/dev/breno5g/laterbox/user/domain/entity/User.java
@@ -0,0 +1,27 @@
+package dev.breno5g.laterbox.user.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String username;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
