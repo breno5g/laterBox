9bb1ed877ec5f6b355779e8256a792e5df638d18
Breno Santos <brenosantos145@gmail.com>
2025-08-08 20:34:36 -0300

    feat: add User authentication with login endpoint and integrate Spring Security's UserDetailsService

--- src/main/java/dev/breno5g/laterbox/user/controller/UserController.java
+++ src/main/java/dev/breno5g/laterbox/user/controller/UserController.java
@@ -1,10 +1,17 @@
 package dev.breno5g.laterbox.user.controller;
 
 import dev.breno5g.laterbox.user.application.dto.CreateUserDTO;
+import dev.breno5g.laterbox.user.application.exceptions.InvalidUsernameOrPasswordException;
 import dev.breno5g.laterbox.user.application.exceptions.UserAlreadyExistsException;
+import dev.breno5g.laterbox.user.application.exceptions.UserExceptions;
 import dev.breno5g.laterbox.user.application.service.UserService;
+import dev.breno5g.laterbox.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +21,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @RequestMapping("/register")
     public ResponseEntity<String> register(@RequestBody CreateUserDTO createUserDTO) throws UserAlreadyExistsException {
         this.userService.createUser(createUserDTO);
         return ResponseEntity.ok("User created successfully");
+    }
+
+    @RequestMapping("/login")
+    public ResponseEntity<String> login(@RequestBody CreateUserDTO request) throws InvalidUsernameOrPasswordException {
+        try {
+            UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.username(), request.password());
+            Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+            User user = (User) authenticate.getPrincipal();
+//          String token = tokenService.generateToken(user);
+            String token = "tokenService.generateToken(user)";
+
+            return ResponseEntity.ok(token);
+        } catch (BadCredentialsException e) {
+            throw UserExceptions.INVALID_USERNAME_OR_PASSWORD_EXCEPTION;
+        }
     }
 }
--- src/main/java/dev/breno5g/laterbox/user/controller/UserExceptionAdvice.java
+++ src/main/java/dev/breno5g/laterbox/user/controller/UserExceptionAdvice.java
@@ -3,6 +3,7 @@ package dev.breno5g.laterbox.user.controller;
 import dev.breno5g.laterbox.user.application.exceptions.UserAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +12,11 @@ public class UserExceptionAdvice {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExistsException ex) {
         return new ResponseEntity<>("message: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UsernameNotFoundException ex) {
+        return new ResponseEntity<>("message: " + ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
