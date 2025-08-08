a3d9b304fa484d56f0bd024e83b6714cb93e0ac5
Breno Santos <brenosantos145@gmail.com>
2025-08-08 15:41:40 -0300

    feat: refactor Link entity and add mappers for DTO conversion

--- src/main/java/dev/breno5g/laterbox/link/application/dto/CreateLinkDTO.java
+++ src/main/java/dev/breno5g/laterbox/link/application/dto/CreateLinkDTO.java
@@ -1,9 +1,11 @@
 package dev.breno5g.laterbox.link.application.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 import java.util.UUID;
 
+@Builder
 public record CreateLinkDTO(
         @NotNull String url,
         @NotNull String title,
---
+++ src/main/java/dev/breno5g/laterbox/link/domain/mapper/LinkMapper.java
@@ -0,0 +1,33 @@
+package dev.breno5g.laterbox.link.domain.mapper;
+
+import dev.breno5g.laterbox.link.application.dto.CreateLinkDTO;
+import dev.breno5g.laterbox.link.application.dto.ResponseLinkDTO;
+import dev.breno5g.laterbox.link.domain.entity.Link;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class LinkMapper {
+    public static Link map(CreateLinkDTO createLinkDTO) {
+        return Link
+                .builder()
+                .title(createLinkDTO.title())
+                .description(createLinkDTO.description())
+                .url(createLinkDTO.url())
+                .userId(createLinkDTO.user_id())
+                .build();
+    }
+
+    public static ResponseLinkDTO map(Link link) {
+        return ResponseLinkDTO
+                .builder()
+                .id(link.getId())
+                .title(link.getTitle())
+                .description(link.getDescription())
+                .createdAt(link.getCreatedAt())
+                .isRead(link.getIsRead())
+                .readAt(link.getReadAt())
+                .isFavorite(link.getIsFavorite())
+                .userId(link.getUserId())
+                .build();
+    }
+}
