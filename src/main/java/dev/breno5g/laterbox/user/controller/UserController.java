8071316986f85326d11533a8e24e26244314d56d
Breno Santos <brenosantos145@gmail.com>
2025-08-09 20:39:27 -0300

    feat: integrate dotenv for environment property loading and add JWT token service with user ID claims

--- src/main/java/dev/breno5g/laterbox/user/controller/UserController.java
+++ src/main/java/dev/breno5g/laterbox/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package dev.breno5g.laterbox.user.controller;
 
+import dev.breno5g.laterbox.config.TokenService;
 import dev.breno5g.laterbox.user.application.dto.CreateUserDTO;
+import dev.breno5g.laterbox.user.application.dto.ResponseUserDTO;
 import dev.breno5g.laterbox.user.application.exceptions.InvalidUsernameOrPasswordException;
 import dev.breno5g.laterbox.user.application.exceptions.UserAlreadyExistsException;
 import dev.breno5g.laterbox.user.application.exceptions.UserExceptions;
@@ -12,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,24 +25,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
-    @RequestMapping("/register")
+    @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody CreateUserDTO createUserDTO) throws UserAlreadyExistsException {
         this.userService.createUser(createUserDTO);
         return ResponseEntity.ok("User created successfully");
     }
 
-    @RequestMapping("/login")
-    public ResponseEntity<String> login(@RequestBody CreateUserDTO request) throws InvalidUsernameOrPasswordException {
+    @PostMapping("/login")
+    public ResponseEntity<ResponseUserDTO> login(@RequestBody CreateUserDTO request) throws InvalidUsernameOrPasswordException {
         try {
             UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.username(), request.password());
             Authentication authenticate = authenticationManager.authenticate(userAndPass);
 
             User user = (User) authenticate.getPrincipal();
-//          String token = tokenService.generateToken(user);
-            String token = "tokenService.generateToken(user)";
 
-            return ResponseEntity.ok(token);
+            String token = this.tokenService.generateToken(user);
+
+            return ResponseEntity.ok(new ResponseUserDTO(token));
         } catch (BadCredentialsException e) {
             throw UserExceptions.INVALID_USERNAME_OR_PASSWORD_EXCEPTION;
         }
