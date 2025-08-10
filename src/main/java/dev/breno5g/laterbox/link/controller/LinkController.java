b57dd5626d4353165e1b8c1c9f2cf946f74c8e0b
Breno Santos <brenosantos145@gmail.com>
2025-08-10 00:14:25 -0300

    feat: update LinkController to utilize LinkService for link creation and ensure proper response mapping

--- src/main/java/dev/breno5g/laterbox/link/controller/LinkController.java
+++ src/main/java/dev/breno5g/laterbox/link/controller/LinkController.java
@@ -2,13 +2,15 @@ package dev.breno5g.laterbox.link.controller;
 
 import dev.breno5g.laterbox.link.application.dto.CreateLinkDTO;
 import dev.breno5g.laterbox.link.application.dto.ResponseLinkDTO;
+import dev.breno5g.laterbox.link.application.exceptions.LinkAlreadyExistsException;
+import dev.breno5g.laterbox.link.application.service.LinkService;
 import dev.breno5g.laterbox.link.domain.entity.Link;
 import dev.breno5g.laterbox.link.domain.mapper.LinkMapper;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/links")
 @RequiredArgsConstructor
 public class LinkController {
+    private final LinkService linkService;
 
     @PostMapping
-    public ResponseEntity<Link> create(@Valid @RequestBody CreateLinkDTO createLinkDTO) {
-        Link responseLinkDTO = LinkMapper.map(createLinkDTO);
-
-        return ResponseEntity.ok(responseLinkDTO);
+    public ResponseEntity<ResponseLinkDTO> create(@Valid @RequestBody CreateLinkDTO createLinkDTO) throws LinkAlreadyExistsException {
+        final Link link = this.linkService.create(createLinkDTO);
+        return ResponseEntity.ok(LinkMapper.map(link));
     }
 }
