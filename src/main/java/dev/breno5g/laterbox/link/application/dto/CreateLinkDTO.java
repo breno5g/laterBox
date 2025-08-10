09d77722964c0771362d426642a08a73ff9b3d80
Breno Santos <brenosantos145@gmail.com>
2025-08-10 00:39:30 -0300

    feat: add tags field to CreateLinkDTO for supporting tag association

--- src/main/java/dev/breno5g/laterbox/link/application/dto/CreateLinkDTO.java
+++ src/main/java/dev/breno5g/laterbox/link/application/dto/CreateLinkDTO.java
@@ -1,11 +1,15 @@
 package dev.breno5g.laterbox.link.application.dto;
 
+import dev.breno5g.laterbox.tag.domain.entity.Tag;
 import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
 
 public record CreateLinkDTO(
         @NotEmpty(message = "Link url must not be empty") String url,
         @NotEmpty(message = "Link title must not be empty") String title,
         @NotEmpty(message = "Link description must not be empty") String description,
+        List<Tag> tags,
         java.util.UUID userId
 ) {
 }
