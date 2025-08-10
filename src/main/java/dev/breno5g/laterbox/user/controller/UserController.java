920689f237b2117a5f685dc319c4958c5453f6f2
Breno Santos <brenosantos145@gmail.com>
2025-08-09 21:19:56 -0300

    feat: add validation for request DTOs and handler for validation exceptions

--- src/main/java/dev/breno5g/laterbox/user/controller/UserController.java
+++ src/main/java/dev/breno5g/laterbox/user/controller/UserController.java
@@ -3,10 +3,10 @@ package dev.breno5g.laterbox.user.controller;
 import dev.breno5g.laterbox.config.TokenService;
 import dev.breno5g.laterbox.user.application.dto.CreateUserDTO;
 import dev.breno5g.laterbox.user.application.dto.ResponseUserDTO;
-import dev.breno5g.laterbox.user.application.exceptions.InvalidUsernameOrPasswordException;
 import dev.breno5g.laterbox.user.application.exceptions.UserAlreadyExistsException;
 import dev.breno5g.laterbox.user.application.service.UserService;
 import dev.breno5g.laterbox.user.domain.entity.User;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -26,13 +26,13 @@ public class UserController {
     private final TokenService tokenService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody CreateUserDTO createUserDTO) throws UserAlreadyExistsException {
+    public ResponseEntity<String> register(@Valid @RequestBody CreateUserDTO createUserDTO) throws UserAlreadyExistsException {
         this.userService.createUser(createUserDTO);
         return ResponseEntity.ok("User created successfully");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<ResponseUserDTO> login(@RequestBody CreateUserDTO request) {
+    public ResponseEntity<ResponseUserDTO> login(@Valid @RequestBody CreateUserDTO request) {
         UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.username(), request.password());
         Authentication authenticate = authenticationManager.authenticate(userAndPass);
 
--- src/main/java/dev/breno5g/laterbox/user/controller/UserExceptionAdvice.java
+++ src/main/java/dev/breno5g/laterbox/user/controller/UserExceptionAdvice.java
@@ -6,8 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class UserExceptionAdvice {
@@ -24,6 +29,16 @@ public class UserExceptionAdvice {
      @ExceptionHandler(BadCredentialsException.class)
      public ResponseEntity<String> handleBadCredentials() {
         return new ResponseEntity<>("message: " + UserExceptions.INVALID_USERNAME_OR_PASSWORD_EXCEPTION.getMessage(), HttpStatus.UNAUTHORIZED);
+     }
+
+     @ExceptionHandler(MethodArgumentNotValidException.class)
+     public ResponseEntity<Map<String, String>> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            errors.put(((FieldError) error).getField(), error.getDefaultMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
      }
 
     @ExceptionHandler(Exception.class)
